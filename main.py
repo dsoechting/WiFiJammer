52e6885b5c32dcb7e82df8fd1d21878d9e28ee2a
Dylan Soechting <dsoechting@gmail.com>
2018-11-15 14:06:24 -0600

    nmap range adjustments

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pyrcrack
 import nmap
 
-ip = '10.202.208.1-30'
-ports = '80'
-nm = nmap.PortScanner()
-nm.scan(ip, ports)
-print("scan info")
-print(nm.scaninfo())
-print("")
-print("hosts:")
-print(nm.all_hosts())
-print("")
-print("hostnames:")
-print(nm[ip].hostnames())
+
+
+def runNmap(ip):
+    # ip = '10.202.208.1-30'
+    ports = '30'
+    nm = nmap.PortScanner()
+    nm.scan(ip, ports)
+    print("scan info")
+    print(nm.scaninfo())
+    print("")
+    print("hosts:")
+    hosts = nm.all_hosts()
+    print(hosts)
+    print("")
+    print("hostnames:")
+    for x in hosts:
+        # tempIP = '10.202.208.' + str((x + 1))
+        print(nm[x].hostname())
+
+runNmap('10.202.208.1-30')
