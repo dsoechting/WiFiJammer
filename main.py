17851b7cce081877bfae708ac164bd8631ebfea8
Evan Heintschel <evanh22@cs.utexas.edu>
2018-11-15 13:29:03 -0600

    basic nmap commmands

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pyrcrack
 import nmap
 
+ip = '10.202.208.1-30'
+ports = '80'
+nm = nmap.PortScanner()
+nm.scan(ip, ports)
+print("scan info")
+print(nm.scaninfo())
+print("")
+print("hosts:")
+print(nm.all_hosts())
+print("")
+print("hostnames:")
+print(nm[ip].hostnames())
