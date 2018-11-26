7b1b3d7ece6adafa8c6a40315603ee4e3c605ae1
dylans <dylans@cs.utexas.edu>
2018-11-26 17:14:12 -0600

    testing

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def deauth():
         print(interface)
         async with pyrcrack.AirodumpNg() as pdump:
             await pdump.run(interface['interface'], write_interval=1)
-            await asyncio.sleep(15)
+            await asyncio.sleep(60)
             print(pdump.get_results())
             # while True:
             #     await asyncio.sleep(3)
